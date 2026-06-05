1df5dc5fe1443fff9a5076afa6b2a3f45fe0644e
Eibon <jackeibon2312@gmail.com>
2026-06-05 10:44:48 -0700

    New Item rework, pulls from database and can be used locally too. Needs testing

--- src/gamelogic/NewItem.java
+++ src/gamelogic/NewItem.java
@@ -1,10 +1,48 @@
 package gamelogic;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.Item;
+import config.DatabaseConnection;
+
 public class NewItem {
-	/*
+	
+	public static Item itemFromDB(String name) throws IOException {
+		Item item= null;
+
+		try (Connection connection = DatabaseConnection.getConnection();
+				Statement st = connection.createStatement();
+				ResultSet rs = st.executeQuery("SELECT * FROM item WHERE item_name = " + name);) 
+		{
+			item= new Item(rs.getInt("id_item"), rs.getString("item_name"),
+						rs.getString("description"),rs.getString("item_tags"));
+			} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return item;
+	}
+	
+	public static Item itemFromDB(int id) throws IOException {
+		Item item= null;
+
+		try (Connection connection = DatabaseConnection.getConnection();
+				Statement st = connection.createStatement();
+				ResultSet rs = st.executeQuery("SELECT * FROM item WHERE id_item = " + id);) 
+		{
+			item= new Item(rs.getInt("id_item"), rs.getString("item_name"),
+						rs.getString("description"),rs.getString("item_tags"));
+			} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return item;
+	}
+	
 	public static Item nothing() {return new Item(
 				0,//id
 				"Nothing",//command name
@@ -12,48 +50,37 @@ public class NewItem {
 				""//description
 				);}
 		
+	/*Local Item generation*/
+	
 	
 	public static Item sword() {
 		Item x=new Item(
 				4,//id
 				"sword",//command name
-				"Rusted Sword", //full name
-				"rustedSword",
 				"A sword eaten by time and air.",//description
 				 new ArrayList<>(List.of(
-				                "rusted",
-				                "weapon",
-				                "metal",
-				                "sharp")));	
+				                "rusted","weapon","sword")));	
 		return x;}
 	
 	public static Item spear() {
 		Item x=new Item(
 				8,//id
 				"spear",//command name
-				"Rusted Spear", //full name
-				"rustedSpear",
 				"A spear eaten by time and air.",//description
 				 new ArrayList<>(List.of(
-				                "rusted",
-				                "weapon",
-				                "long",
-				                "sharp")));	
+				                "spear,weapon,rusted,pole,reach,thrown,melee")));	
 		return x;}
 	
 	public static Item gun() {
 		Item x=new Item(
 				11,//id
 				"gun",//command name
-				"Rusted gun", //full name
-				"rustedGun",
 				"A gun eaten by time and air.",//description
 				 new ArrayList<>(List.of(
-				                "rusted",
-				                "weapon",
-				                "firearm",
-				                "projectile")));	
+				                "gun,weapon,projectile,ranged,gunpowder,rusted")));	
 		return x;}
 		
-*/
+
+	
+	
 }
